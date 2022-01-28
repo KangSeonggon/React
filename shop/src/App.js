93dd8a64085614b37c4e6871efaaaf395dfdafa5
goni111 <kangsk94@gmail.com>
2022-01-28 17:03:58 +0900

    App.js / loading, axios

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,16 +1,16 @@
 /*eslint-disable*/ 
 import './App.css';
-import {Navbar,Nav,NavDropdown,Container } from 'react-bootstrap';
-import React, { useState } from 'react';
+import {Navbar,Nav,NavDropdown,Container, Button, Spinner } from 'react-bootstrap';
+import React, { useEffect, useReducer, useState } from 'react';
 import {Link, Route, Switch} from 'react-router-dom';
-
+import axios from 'axios';
 
 import Detail from "./detail";
 import data from './data.js';
 
 function App() {
   let [shoes ,c_shoes] = useState(data);
-
+  let [load , c_load] = useState(false);
 
   return (
     <div className="App">
@@ -40,8 +40,27 @@ function App() {
         </div>
         <div className='container'>
         <div className='row'>
-          <Card shoes={shoes} />
+          {
+            shoes.map((a,i) => {
+              return <Card2 shoes = {shoes[i]} i={i} key={i} />
+            })
+          }
         </div>
+        {/* html 을 추가할생각하지말고 card2 컴포넌트 shoes state에 데이터를 추가할 생각해라 */}
+        <button className="btn btn-primary" onClick={()=>{
+          c_load(true)
+          axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then((result) => {
+            c_shoes([...shoes, ...result.data])
+            c_load(false)
+          })
+          .catch(() => { c_load(false) })
+        }}>더보기</button>
+        {
+            load === true
+            ? (<Loading/>)
+            : null
+        }
       </div>
       </Route>
       <Route exact path="/detail">
@@ -66,7 +85,7 @@ function App() {
     </div>
   );
 }
-
+// 내방법
 function Card(props) {
   let [imgurl,c_imgurl] = useState(["https://codingapple1.github.io/shop/shoes1.jpg","https://codingapple1.github.io/shop/shoes2.jpg","https://codingapple1.github.io/shop/shoes3.jpg"])
   return(
@@ -76,11 +95,38 @@ function Card(props) {
           <img src={imgurl[i]} width="100%" key={i} />
           <h2> {a.title} </h2>
           <p> {a.content} / {a.price}</p>
+        
         </div>
       )
     })
+    )
+}
+// 강의방법
+function Card2(props){
+  return (
+    <div className="col-md-4">
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"/>
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content } & { props.shoes.price }</p>
+      
+    </div>
   )
 }
+
+function Loading() {
+ return (
+    <Button variant="primary" disabled>
+      <Spinner
+        as="span"
+        animation="grow"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+      Loading...
+    </Button>
+ )}
+ 
 
 
 
